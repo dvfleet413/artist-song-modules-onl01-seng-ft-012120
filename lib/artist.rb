193b81affe357b432581e98f58dae4af072e2141
David Van Fleet <dvfleet413@gmail.com>
2020-02-02 03:49:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,8 +3,8 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable
-  extend Findable
+  extend Memorable, Findable
+#  extend Findable
 
   @@artists = []
 
